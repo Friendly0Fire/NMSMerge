354341ac832d3dcb45743cbd7a40d1c5df775917
FriendlyFire <jpguertin.cg@gmail.com>
2019-09-01 17:25:56 -0400

    Started working on parsing PAKs.

--- props/Settings.Designer.cs
+++ props/Settings.Designer.cs
@@ -46,5 +46,16 @@ namespace NMSMerge {
                 this["ModsFolder"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public global::System.Collections.Specialized.StringCollection ActiveMods {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["ActiveMods"]));
+            }
+            set {
+                this["ActiveMods"] = value;
+            }
+        }
     }
 }
---
+++ src/MBINDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libMBIN;
+using PSArcHandler;
+using System.IO;
+
+namespace NMSMerge
+{
+    class MBINDatabase
+    {
+        public delegate MBINFile LoadMBINDelegate(PSARC pak, int offset);
+
+        public Dictionary<string, PSARC> Paks { get; } = new Dictionary<string, PSARC>();
+        public Dictionary<string, LoadMBINDelegate> LazyMBINs { get; } = new Dictionary<string, LoadMBINDelegate>();
+
+        public void LoadMINs(string folder) => LoadMBINs(Directory.EnumerateFiles(folder, "*.pak"));
+
+        public void LoadMBINs(IEnumerable<string> paths)
+        {
+            foreach(var pakPath in paths)
+            {
+                var pak = new PSARC(pakPath);
+                Paks[pakPath] = pak;
+                foreach (var entry in pak.TOC)
+                {
+                    if (Path.GetExtension(entry.FileName)?.ToLower() == ".mbin")
+                        LazyMBINs[entry.FileName] = delegate
+                        {
+                            var decompressed = pak.DecompressFile(entry.FileName);
+                            var stream = new MemoryStream(decompressed.BinaryFile);
+                            return new MBINFile(stream, true);
+                        };
+                }
+            }
+        }
+    }
+}
